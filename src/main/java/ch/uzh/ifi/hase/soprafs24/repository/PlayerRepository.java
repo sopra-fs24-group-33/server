840633d4d52454363c67e070a5c7065eb5279f24
kungfuvali <160481944+kungfuvali@users.noreply.github.com>
2024-04-11 11:53:37 +0200

    Add files via upload

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package ch.uzh.ifi.hase.soprafs24.repository;
+
+import ch.uzh.ifi.hase.soprafs24.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("guestRepository")
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
