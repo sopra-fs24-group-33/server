7bb1808b0af8241aaa4482d051d6b738caf8b7bf
kungfuvali <160481944+kungfuvali@users.noreply.github.com>
2024-04-11 11:54:56 +0200

    Add files via upload

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/GameLobbyGetDTO.java
@@ -0,0 +1,50 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Game;
+import ch.uzh.ifi.hase.soprafs24.entity.GamePlayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameLobbyGetDTO {
+        private Long admin;
+
+        private int pin;
+
+        private List<GamePlayer> gamePlayers = new ArrayList<>();
+
+        private Game gamestatus;
+
+
+        public Long getAdmin() {
+            return admin;
+        }
+
+        public void setAdmin(Long admin) {
+            this.admin = admin;
+        }
+
+        public int getPin() {
+            return pin;
+        }
+
+        public void setPin(int pin) {
+            this.pin = pin;
+        }
+
+    public List<GamePlayer> getPlayers() {
+        return gamePlayers;
+    }
+
+    public void setPlayers(List<GamePlayer> gamePlayers) {
+        this.gamePlayers = gamePlayers;
+    }
+
+    public Game getGamestatus() {
+        return gamestatus;
+    }
+
+    public void setGamestatus(Game gamestatus) {
+        this.gamestatus = gamestatus;
+    }
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/PlayerPostDTO.java
@@ -0,0 +1,50 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
+
+public class PlayerPostDTO {
+    private Long id;
+    private String name;
+    private int shame_tokens;
+    private String token;
+    private UserStatus status;
+    private Long isUser;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getShame_tokens() {
+        return shame_tokens;
+    }
+    public void setShame_tokens(int shame_tokens) {
+        this.shame_tokens = shame_tokens;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setToken(String token)  {
+        this.token = token;
+    }
+    public String getToken()    {
+        return token;
+    }
+    public UserStatus getStatus() {
+        return status;
+    }
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    public Long getIsUser() {
+        return isUser;
+    }
+
+    public void setIsUser(Long user) {
+        this.isUser = user;
+    }
+}
