1a70a8b07a12d71e0d95f4d5a6336ca9e524ca0f
kungfuvali <valentinpeter.ambass@uzh.ch>
2024-04-26 17:40:03 +0200

    GameLobbyServicetest
    Fixes #85

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/GameLobbyServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/GameLobbyServiceTest.java
@@ -33,27 +33,18 @@ public class GameLobbyServiceTest {
 
     @Test
     public void createGameLobby_validInputs_success() {
-        // Given
         Player admin = new Player();
         admin.setId(1L);
         admin.setName("Admin");
 
         GameLobby savedGameLobby = new GameLobby();
-        savedGameLobby.setPin(123456); // Set a known pin for verification
-
-        // Mock behavior of the repository
+        savedGameLobby.setPin(111111);
         when(gamelobbyRepository.save(any(GameLobby.class))).thenReturn(savedGameLobby);
-
-        // When
         GameLobby createdGameLobby = gamelobbyService.createGameLobby(admin);
-
-        // Then
         assertNotNull(createdGameLobby);
         assertEquals(admin.getId(), createdGameLobby.getAdmin());
         assertNotNull(createdGameLobby.getPin());
         assertNotEquals(0, createdGameLobby.getPin());
-
-        // Verify that save method of repository was called with the created GameLobby
         verify(gamelobbyRepository, times(1)).save(any(GameLobby.class));
     }
 }
