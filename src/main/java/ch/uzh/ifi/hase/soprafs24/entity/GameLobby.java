cad26cea8fe3e4df9b89594db332da4befa0b1f8
Kavishan Srirangarasa <kavishanwin8@hotmail.com>
2024-05-21 13:45:45 +0200

    changed Set back to List
    in order to have the gamePlayers list ordered when someone joins/leaves

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/GameLobby.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/GameLobby.java
@@ -3,36 +3,36 @@ package ch.uzh.ifi.hase.soprafs24.entity;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Objects;
 
 @Entity
 @Table(name = "GAMELOBBY")
 public class GameLobby implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Column
-    private Long admin;
+	@Column
+	private Long admin;
 
 	@Id
-    private Integer pin;
+	private Integer pin;
 
-    @Column
-    private Long gameid;
+	@Column
+	private Long gameid;
 
-    @OneToMany(mappedBy = "gameLobby", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<GamePlayer> gamePlayers = new HashSet<>();
+	@OneToMany(mappedBy = "gameLobby", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@OrderColumn(name = "order_column") // This annotation ensures the order is persisted in the database
+	private List<GamePlayer> gamePlayers = new ArrayList<>();
 
-    public void addPlayer(GamePlayer gamePlayer) {
-        gamePlayers.add(gamePlayer);
-        gamePlayer.setGameLobby(this);
-    }
+	public void addPlayer(GamePlayer gamePlayer) {
+		gamePlayers.add(gamePlayer);
+		gamePlayer.setGameLobby(this);
+	}
 
     public void removePlayer(Long id) {
         for (GamePlayer player : this.getPlayers())  {
-            if (player.getId() == id)   {
+            if (Objects.equals(player.getId(), id))   {
                 gamePlayers.remove(player);
                 player.setGameLobby(null);
                 break;
@@ -40,43 +40,43 @@ public class GameLobby implements Serializable {
         }
     }
 
-    public Long getAdmin() {
-        return admin;
-    }
+	public Long getAdmin() {
+		return admin;
+	}
 
-    public void setAdmin(Long admin) {
-        this.admin = admin;
-    }
+	public void setAdmin(Long admin) {
+		this.admin = admin;
+	}
 
-    public int getPin() {
-        return pin;
-    }
+	public int getPin() {
+		return pin;
+	}
 
-    public void setPin(int pin) {
-        this.pin = pin;
-    }
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
 
-    public Set<GamePlayer> getPlayers() {
-        return gamePlayers;
-    }
+	public List<GamePlayer> getPlayers() {
+		return gamePlayers;
+	}
 
-    public void setPlayers(Set<GamePlayer> gamePlayers) {
-        this.gamePlayers = gamePlayers;
-    }
+	public void setPlayers(List<GamePlayer> gamePlayers) {
+		this.gamePlayers = gamePlayers;
+	}
 
-    public Long getGameid() {
-        return gameid;
-    }
+	public Long getGameid() {
+		return gameid;
+	}
 
-    public void setGameid(Long gameid) {
-        this.gameid = gameid;
-    }
+	public void setGameid(Long gameid) {
+		this.gameid = gameid;
+	}
 
-    public Set<GamePlayer> getGamePlayers() {
-        return gamePlayers;
-    }
+	public List<GamePlayer> getGamePlayers() {
+		return gamePlayers;
+	}
 
-    public void setGamePlayers(Set<GamePlayer> gamePlayers) {
-        this.gamePlayers = gamePlayers;
-    }
+	public void setGamePlayers(List<GamePlayer> gamePlayers) {
+		this.gamePlayers = gamePlayers;
+	}
 }
